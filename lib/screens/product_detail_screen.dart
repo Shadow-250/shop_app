e63a5ff6d0003fc70113195eb759f1a084e5803e
Shadow-250 <nhphung10499@gmail.com>
2020-02-05 20:53:16 +0700

    Test

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:shop_app/provider/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -10,6 +13,7 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final producId = ModalRoute.of(context).settings.arguments as String; // get id
+    Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),
